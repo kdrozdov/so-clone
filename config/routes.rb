fc1d7617c1cc9a99118cb155e80225a9ea085736
kirill-dzv <kirill.dzv@gmail.com>
2014-11-06 17:57:00 +0500

    Add doorkeeper

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  use_doorkeeper
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
 
   get '/users/:id/finish_signup', to: 'users#finish_signup', as: 'finish_signup'
